4125999e3923800cd8256fc73c7b0fa1b42dfd98
LorenzoRegalzi <lorenzo.regalzi@gmail.com>
2024-03-10 17:17:24 +0100

    map exercise

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,35 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import { User } from './model/user';
+import { map } from 'rxjs';
 
 @Component({
   imports: [HttpClientModule],
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
+    <h1>RxJs - chiamata al Server !</h1>
     <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
+    {{ name }}
     </a>
   `,
 })
 export class App {
   name = 'Angular';
+
+  constructor(
+    private http: HttpClient
+  ){
+    this.http.get<User>('https://jsonplaceholder.typicode.com/users/1')
+    .pipe(
+      map(val => val.email)
+    )
+    .subscribe(res => 
+      this.name = res
+    )
+  }
 }
 
 bootstrapApplication(App);
