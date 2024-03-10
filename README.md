487167628eb534cbef3fb5011eb5ebc4544de078
LorenzoRegalzi <59871716+LorenzoRegalzi@users.noreply.github.com>
2024-03-10 17:05:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rx-js-angular
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/LorenzoRegalzi/rx-js-angular)
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+
+@Component({
+  imports: [HttpClientModule],
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+    <a target="_blank" href="https://angular.dev/overview">
+      Learn more about Angular
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
